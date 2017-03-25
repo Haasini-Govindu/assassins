f9e24e152f2ce71fbf291d956691aba50a6cf1b9
rju <rju@purdue.edu>
2017-03-25 14:12:53 -0400

    startGame properly creates Checking Accounts for players

--- targets.py
+++ targets.py
@@ -4,6 +4,13 @@
 from random import shuffle, randint
 from nessie import MasterAccount
 
+def generateCode(n):
+	string = ''
+	for i in range(0, n):
+		r = randint(0, 9)
+		string += str(r)
+	return string
+
 class Player:
 	def __init__(self, name, number):
 		self.name = name
@@ -60,14 +67,7 @@ class Game:
 		print '----------'
 		for p in self.players:
 			print p
-		print '----------'
-
-	def generateCode(self, n):
-		string = ''
-		for i in range(0, n):
-			r = randint(0, 9)
-			string += str(r)
-		return string		
+		print '----------'		
 
 	def startGame(self):
 		acc = MasterAccount()
@@ -77,11 +77,12 @@ class Game:
 			self.players[n].setSecretCode(generateCode(6))
 			self.players[n].set_id(acc.createAccount(self.players[n].getName()))
 			self.players[n].setTarget(self.players[n+1])
-			print str(self.players[n].getName()) + ' is targeting ' + str(self.players[n].getTarget())
+# 			print str(self.players[n].getName()) + ' is targeting ' + str(self.players[n].getTarget())
 		
 		self.players[len(self.players)-1].setSecretCode(generateCode(6))
+		self.players[len(self.players)-1].set_id(acc.createAccount(self.players[len(self.players)-1].getName()))
 		self.players[len(self.players)-1].setTarget(self.players[0])
-		print str(self.players[len(self.players)-1].getName()) + ' is targeting ' + str(self.players[len(self.players)-1].getTarget())
+# 		print str(self.players[len(self.players)-1].getName()) + ' is targeting ' + str(self.players[len(self.players)-1].getTarget())
 	
 g = Game()
 g.addPlayer(Player('Ahri', '+18002221222'))
@@ -90,8 +91,8 @@ g.addPlayer(Player('Kha\'zix', '+18002221222'))
 g.addPlayer(Player('Rengar', '+18002221222'))
 g.addPlayer(Player('Talon', '+18002221222'))
 g.addPlayer(Player('Zed', '+18002221222'))
-
-
+g.printGameStatus()
+g.startGame()
 
 # assassinate(players[2], players[2].getTarget())
 # assassinate(players[4], players[4].getTarget())
